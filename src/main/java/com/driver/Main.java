cd217c7ac16c721a56dc4edd60d556149ac9d88e
ajayv28 <ajayvengateshbabu@gmail.com>
2024-08-03 00:00:41 +0300

    final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,20 @@ package com.driver;
 
 public class Main {
 
+    public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A {
+
+        public String meth(){
+            return "Method is overridden in Extended class B";
+        }
+
+    }
+
     public static void main(String[] args) {
         B C = new B();
         System.out.println(C.meth());
