314fedb1a7f4d062c7694e5833e57645af4a8a37
ajayv28 <ajayvengateshbabu@gmail.com>
2024-08-02 22:39:24 +0300

    final 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,7 @@ package com.driver;
 public class Main {
 
     public static void main(String[] args) {
-        A D = new B();
-        D.meth();
-
         B C = new B();
-        C.meth();
+        System.out.println(C.meth());
     }
 }
