f96f2ce008f98c2fabe64cad9f406d5d493288a6
ajayv28 <ajayvengateshbabu@gmail.com>
2024-08-02 22:53:01 +0300

    bnm

---
+++ src/test/java/com/driver/test/A.java
@@ -0,0 +1,7 @@
+package com.driver.test;
+
+public class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
---
+++ src/test/java/com/driver/test/B.java
@@ -0,0 +1,12 @@
+package com.driver.test;
+
+import com.driver.A;
+
+public class B extends A {
+
+
+    public String meth(){
+        return "Method is overridden in Extended class B";
+    }
+
+}
